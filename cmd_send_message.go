31e4829a06d457f2577477d0670a92a0f60142cb
Bougou <bougou@126.com>
2022-01-03 17:16:47 +0800

    Add SendMessage command

--- cmd_send_message.go
+++ cmd_send_message.go
@@ -2,7 +2,11 @@ package ipmi
 
 // 22.7 Send Message Command
 type SendMessageRequest struct {
-	Tracked bool
+	// [7:6] 00b = No tracking
+	// 01b = Track Request.
+	// 10b = Send Raw. (optional)
+	// 11b = reserved
+	TrackMask uint8
 
 	Encrypted bool
 
@@ -10,22 +14,46 @@ type SendMessageRequest struct {
 
 	ChannelNumber uint8
 
-	Data []byte
+	// Todo
+	MessageData []byte
 }
 
 type SendMessageResponse struct {
-	NetFn
-
-	LUN
-
-	Command
-
-	CompletionCode
-
-	// 	Data []byte
+	// This data will only be present when using the Send Message command to
+	// originate requests from IPMB or PCI Management Bus to other channels
+	// such as LAN or serial/modem. It is not present in the response to a
+	// Send Message command delivered via the System Interface.
+	Data []byte
 }
 
-func (r *SendMessageResponse) CompletionCodes() map[uint8]string {
+func (req SendMessageRequest) Command() Command {
+	return CommandSendMessage
+}
+
+func (req *SendMessageRequest) Pack() []byte {
+	out := make([]byte, 1+len(req.MessageData))
+
+	var b uint8 = req.ChannelNumber
+	if req.Authenticated {
+		b = setBit4(b)
+	}
+	if req.Encrypted {
+		b = setBit5(b)
+	}
+	b |= (req.TrackMask << 6)
+
+	packUint8(b, out, 0)
+	packBytes(req.MessageData, out, 1)
+
+	return out
+}
+
+func (res *SendMessageResponse) Unpack(msg []byte) error {
+	res.Data, _, _ = unpackBytes(msg, 0, len(msg))
+	return nil
+}
+
+func (*SendMessageResponse) CompletionCodes() map[uint8]string {
 	return map[uint8]string{
 		0x80: "Invalid Session Handle. The session handle does not match up with any currently active sessions for this channel.",
 		0x81: "Lost Arbitration",
@@ -34,5 +62,19 @@ func (r *SendMessageResponse) CompletionCodes() map[uint8]string {
 	}
 }
 
-func (c *Client) SendMessage() {
+func (res *SendMessageResponse) Format() string {
+	return ""
+}
+
+func (c *Client) SendMessage(channelNumber uint8, authenticated bool, encrypted bool, trackMask uint8, data []byte) (response *SendMessageResponse, err error) {
+	request := &SendMessageRequest{
+		ChannelNumber: channelNumber,
+		Authenticated: authenticated,
+		Encrypted:     encrypted,
+		TrackMask:     trackMask,
+		MessageData:   data,
+	}
+	response = &SendMessageResponse{}
+	err = c.Exchange(request, response)
+	return
 }
