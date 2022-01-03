811428389a7c21dad18caa71d6ea01392e1cbad6
Bougou <bougou@126.com>
2022-01-03 17:01:13 +0800

    Add GetMessage command

--- cmd_get_message.go
+++ cmd_get_message.go
@@ -1,16 +1,45 @@
 package ipmi
 
-type GeMessageRequest struct {
+// 22.6 Get Message Command
+type GetMessageRequest struct {
+	// empty
 }
 
 type GetMessageResponse struct {
-	NetFn
+	ChannelNumber uint8
+	MessageData   []byte
+}
 
-	LUN
+func (req *GetMessageRequest) Command() Command {
+	return CommandGetMessage
+}
 
-	Command
+func (req *GetMessageRequest) Pack() []byte {
+	return []byte{}
+}
 
-	CompletionCode
+func (res *GetMessageResponse) CompletionCodes() map[uint8]string {
+	return map[uint8]string{
+		0x80: "data not available (queue / buffer empty)",
+	}
+}
 
-	Channel
+func (res *GetMessageResponse) Unpack(msg []byte) error {
+	if len(msg) < 1 {
+		return ErrUnpackedDataTooShort
+	}
+	res.ChannelNumber, _, _ = unpackUint8(msg, 0)
+	res.MessageData, _, _ = unpackBytes(msg, 1, len(msg)-1)
+	return nil
+}
+
+func (res *GetMessageResponse) Format() string {
+	return ""
+}
+
+func (c *Client) GetMessage() (response *GetMessageResponse, err error) {
+	request := &GetMessageRequest{}
+	response = &GetMessageResponse{}
+	err = c.Exchange(request, response)
+	return
 }
