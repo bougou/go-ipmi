4392b066b2ffc2d37f0d7aa183654e90c371f7b8
Bougou <bougou@126.com>
2022-09-29 13:43:14 +0800

    feat(cmd): improve fru print cmd

--- goipmi/commands/fru.go
+++ goipmi/commands/fru.go
@@ -19,24 +19,40 @@ func NewCmdFRU() *cobra.Command {
 			return closeClient()
 		},
 	}
-	cmd.AddCommand(NewCmdFRUList())
+	cmd.AddCommand(NewCmdFRUPrint())
 
 	return cmd
 }
 
-func NewCmdFRUList() *cobra.Command {
+func NewCmdFRUPrint() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "list",
-		Short: "list",
+		Use:   "print",
+		Short: "print",
 		Run: func(cmd *cobra.Command, args []string) {
-			frus, err := client.GetFRUs()
-			if err != nil {
-				CheckErr(fmt.Errorf("GetFRUs failed, err: %s", err))
-			}
 
-			for _, fru := range frus {
+			if len(args) < 1 {
+				frus, err := client.GetFRUs()
+				if err != nil {
+					CheckErr(fmt.Errorf("GetFRUs failed, err: %s", err))
+				}
+
+				for _, fru := range frus {
+					fmt.Println(fru.String())
+				}
+			} else {
+				id, err := parseStringToInt64(args[0])
+				if err != nil {
+					CheckErr(fmt.Errorf("invalid FRU Device ID passed, err: %s", err))
+				}
+				fruID := uint8(id)
+
+				fru, err := client.GetFRU(fruID, "")
+				if err != nil {
+					CheckErr(fmt.Errorf("GetFRU failed, err: %s", err))
+				}
 				fmt.Println(fru.String())
 			}
+
 		},
 	}
 	return cmd
