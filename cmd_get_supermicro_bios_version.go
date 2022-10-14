28e1be9e441c174fcbed82776800497b8f258fe6
Danny Gowing <danny.gowing@unity3d.com>
2022-10-14 18:39:03 +0100

    Add Supermicro bios version command

---
+++ cmd_get_supermicro_bios_version.go
@@ -0,0 +1,44 @@
+package ipmi
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CommandGetSupermicroBiosVersionRequest struct {
+}
+
+type CommandGetSupermicroBiosVersionResponse struct {
+	Version string
+}
+
+func (req *CommandGetSupermicroBiosVersionRequest) Command() Command {
+	return CommandGetSupermicroBiosVersion
+}
+
+func (req *CommandGetSupermicroBiosVersionRequest) Pack() []byte {
+	return []byte{0x00, 0x00}
+}
+
+func (res *CommandGetSupermicroBiosVersionResponse) Unpack(msg []byte) error {
+	res.Version = string(msg)
+	res.Version = strings.TrimSpace(res.Version)
+	return nil
+}
+
+func (res *CommandGetSupermicroBiosVersionResponse) CompletionCodes() map[uint8]string {
+	return map[uint8]string{}
+}
+
+func (res *CommandGetSupermicroBiosVersionResponse) Format() string {
+	return fmt.Sprintf(`bios.version = %s`,
+		res.Version,
+	)
+}
+
+func (c *Client) GetSupermicroBiosVersion() (response *CommandGetSupermicroBiosVersionResponse, err error) {
+	request := &CommandGetSupermicroBiosVersionRequest{}
+	response = &CommandGetSupermicroBiosVersionResponse{}
+	err = c.Exchange(request, response)
+	return
+}
