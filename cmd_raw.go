b7dd375594178ada365455ae8fcf34b8f4b290ca
Mark Horn <mark.d.horn@intel.com>
2023-07-01 15:36:40 -0700

    Add support for Raw IPMI Commands
    Signed-off-by: Mark Horn <mark.d.horn@intel.com>

---
+++ cmd_raw.go
@@ -0,0 +1,63 @@
+package ipmi
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CommandRawRequest struct {
+	NetFn NetFn
+	Cmd   uint8
+	Data  []byte
+	Name  string
+}
+
+type CommandRawResponse struct {
+	Response []byte
+}
+
+func (req *CommandRawRequest) Command() Command {
+	return Command{ID: req.Cmd, NetFn: req.NetFn, Name: req.Name}
+}
+
+func (req *CommandRawRequest) Pack() []byte {
+	out := make([]byte, len(req.Data))
+
+	packBytes(req.Data, out, 0)
+
+	return out
+}
+
+func (res *CommandRawResponse) Unpack(msg []byte) error {
+	res.Response = msg
+	return nil
+}
+
+func (res *CommandRawResponse) CompletionCodes() map[uint8]string {
+	return map[uint8]string{}
+}
+
+func (res *CommandRawResponse) Format() string {
+	// convert the byte array to a slice of hex strings
+	hexStrings := make([]string, len(res.Response))
+	for i, b := range res.Response {
+		hexStrings[i] = fmt.Sprintf("0x%02X", b)
+	}
+
+	// join the hex strings with commas
+	hexString := strings.Join(hexStrings, ", ")
+
+	return fmt.Sprintf(`raw.Response = %s`, hexString)
+}
+
+func (c *Client) RawCommand(netFn NetFn, cmd uint8, data []byte, name string) (response *CommandRawResponse, err error) {
+	request := &CommandRawRequest{
+		NetFn: netFn,
+		Cmd:   cmd,
+		Data:  data,
+		Name:  name,
+	}
+	response = &CommandRawResponse{}
+	err = c.Exchange(request, response)
+	return
+}
