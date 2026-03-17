cff95542b1792b1d12e7df504b22d8fbf616e13d
Bougou <bougou@126.com>
2026-03-17 11:27:16 +0800

    Refactor OEM constants and string mapping for improved readability and consistency

--- types_oem.go
+++ types_oem.go
@@ -5,105 +5,105 @@ type OEM uint32
 
 // cSpell: disable
 const (
-	OEM_UNKNOWN                      = 0
-	OEM_DEBUG                        = 0xFFFFFE /* Hoping IANA won't hit this soon */
-	OEM_RESERVED                     = 0x0FFFFF /* As per IPMI 2.0 specification */
-	OEM_IBM_2                        = 2        /* 2 for [IBM] */
-	OEM_HP                           = 11
-	OEM_SUN                          = 42
-	OEM_NOKIA                        = 94
-	OEM_BULL                         = 107
-	OEM_HITACHI_116                  = 116
-	OEM_NEC                          = 119
-	OEM_TOSHIBA                      = 186
-	OEM_ERICSSON                     = 193
-	OEM_INTEL                        = 343
-	OEM_TATUNG                       = 373
-	OEM_HITACHI_399                  = 399
-	OEM_DELL                         = 674
-	OEM_HUAWEI                       = 2011
-	OEM_LMC                          = 2168
-	OEM_ASUSTEK                      = 2623
-	OEM_RADISYS                      = 4337
-	OEM_BROADCOM                     = 4413
-	OEM_IBM_4769                     = 4769 /* 4769 for [IBM Corporation] */
-	OEM_MAGNUM                       = 5593
-	OEM_TYAN                         = 6653
-	OEM_QUANTA                       = 7244
-	OEM_VIKING                       = 9237
-	OEM_ADVANTECH                    = 10297
-	OEM_FUJITSU_SIEMENS              = 10368
-	OEM_AVOCENT                      = 10418
-	OEM_PEPPERCON                    = 10437
-	OEM_SUPERMICRO                   = 10876
-	OEM_OSA                          = 11102
-	OEM_GOOGLE                       = 11129
-	OEM_PICMG                        = 12634
-	OEM_RARITAN                      = 13742
-	OEM_KONTRON                      = 15000
-	OEM_PPS                          = 16394
-	OEM_IBM_20301                    = 20301 /* 20301 for [IBM eServer X] */
-	OEM_AMI                          = 20974
-	OEM_FOXCONN                      = 22238
-	OEM_ADLINK_24339                 = 24339 /* 24339 for [ADLINK TECHNOLOGY INC.] */
-	OEM_H3C                          = 25506
-	OEM_NOKIA_SOLUTIONS_AND_NETWORKS = 28458
-	OEM_VITA                         = 33196
-	OEM_INSPUR                       = 37945
-	OEM_TENCENT                      = 41475
-	OEM_BYTEDANCE                    = 46045
-	OEM_SUPERMICRO_47488             = 47488
-	OEM_YADRO                        = 49769
+	OEM_UNKNOWN          = 0
+	OEM_DEBUG            = 0xFFFFFE /* Hoping IANA won't hit this soon */
+	OEM_RESERVED         = 0x0FFFFF /* As per IPMI 2.0 specification */
+	OEM_IBM_2            = 2        /* 2 for [IBM] */
+	OEM_HP               = 11
+	OEM_SUN              = 42
+	OEM_NOKIA            = 94
+	OEM_BULL             = 107
+	OEM_HITACHI_116      = 116
+	OEM_NEC              = 119
+	OEM_TOSHIBA          = 186
+	OEM_ERICSSON         = 193
+	OEM_INTEL            = 343
+	OEM_TATUNG           = 373
+	OEM_HITACHI_399      = 399
+	OEM_DELL             = 674
+	OEM_HUAWEI           = 2011
+	OEM_LMC              = 2168
+	OEM_ASUSTEK          = 2623
+	OEM_RADISYS          = 4337
+	OEM_BROADCOM         = 4413
+	OEM_IBM_4769         = 4769 /* 4769 for [IBM Corporation] */
+	OEM_MAGNUM           = 5593
+	OEM_TYAN             = 6653
+	OEM_QUANTA           = 7244
+	OEM_VIKING           = 9237
+	OEM_ADVANTECH        = 10297
+	OEM_FUJITSU_SIEMENS  = 10368
+	OEM_AVOCENT          = 10418
+	OEM_PEPPERCON        = 10437
+	OEM_SUPERMICRO       = 10876
+	OEM_OSA              = 11102
+	OEM_GOOGLE           = 11129
+	OEM_PICMG            = 12634
+	OEM_RARITAN          = 13742
+	OEM_KONTRON          = 15000
+	OEM_PPS              = 16394
+	OEM_IBM_20301        = 20301 /* 20301 for [IBM eServer X] */
+	OEM_AMI              = 20974
+	OEM_FOXCONN          = 22238
+	OEM_ADLINK_24339     = 24339 /* 24339 for [ADLINK TECHNOLOGY INC.] */
+	OEM_H3C              = 25506
+	OEM_NOKIA_28458      = 28458
+	OEM_VITA             = 33196
+	OEM_INSPUR           = 37945
+	OEM_TENCENT          = 41475
+	OEM_BYTEDANCE        = 46045
+	OEM_SUPERMICRO_47488 = 47488
+	OEM_YADRO            = 49769
 )
 
 func (oem OEM) String() string {
 	m := map[OEM]string{
-		2:     "IBM",
-		11:    "HP",
-		42:    "Sun",
-		94:    "Nokia",
-		107:   "Bull",
-		116:   "Hitachi",
-		119:   "NEC",
-		186:   "Toshiba",
-		193:   "Ericsson",
-		343:   "Intel",
-		373:   "Tatung", // 大同
-		399:   "Hitachi",
-		674:   "Dell",
-		2011:  "Huawei",
-		2168:  "LMC",
-		2623:  "ASUSTek",
-		4337:  "Radisys",
-		4413:  "Broadcom",
-		4769:  "IBM",
-		5593:  "Magnum", // 迈格纳技术集成公司
-		6653:  "Tyan",   // 泰安
-		7244:  "Quanta",
-		9237:  "Viking",
-		10297: "Advantech", // 研华科技
-		10368: "Fujitsu",
-		10418: "Avocent",
-		10437: "Peppercon",
-		10876: "Supermicro",
-		11102: "OSA",
-		11129: "Google",
-		12634: "PICMG",
-		13742: "Raritan", // 力登
-		15000: "Kontron", // 控创
-		16394: "PPS",
-		20301: "IBM",
-		20974: "AMI",
-		22238: "Foxconn",
-		24339: "ADLINK", // 凌华
-		25506: "H3C",
-		28458: "Nokia",
-		33196: "Vita",      // 维塔
-		37945: "Inspur",    // 浪潮
-		41475: "Tencent",   // 腾讯
-		46045: "ByteDance", // 字节跳动
-		47488: "Supermicro",
-		49769: "Yadro",
+		OEM_IBM_2:            "IBM",
+		OEM_HP:               "HP",
+		OEM_SUN:              "Sun",
+		OEM_NOKIA:            "Nokia",
+		OEM_BULL:             "Bull",
+		OEM_HITACHI_116:      "Hitachi",
+		OEM_NEC:              "NEC",
+		OEM_TOSHIBA:          "Toshiba",
+		OEM_ERICSSON:         "Ericsson",
+		OEM_INTEL:            "Intel",
+		OEM_TATUNG:           "Tatung", // 大同
+		OEM_HITACHI_399:      "Hitachi",
+		OEM_DELL:             "Dell",
+		OEM_HUAWEI:           "Huawei",
+		OEM_LMC:              "LMC",
+		OEM_ASUSTEK:          "Asustek",
+		OEM_RADISYS:          "Radisys",
+		OEM_BROADCOM:         "Broadcom",
+		OEM_IBM_4769:         "IBM",
+		OEM_MAGNUM:           "Magnum", // 迈格纳技术集成公司
+		OEM_TYAN:             "Tyan",   // 泰安
+		OEM_QUANTA:           "Quanta",
+		OEM_VIKING:           "Viking",
+		OEM_ADVANTECH:        "Advantech", // 研华科技
+		OEM_FUJITSU_SIEMENS:  "Fujitsu",
+		OEM_AVOCENT:          "Avocent",
+		OEM_PEPPERCON:        "Peppercon",
+		OEM_SUPERMICRO:       "Supermicro",
+		OEM_OSA:              "OSA",
+		OEM_GOOGLE:           "Google",
+		OEM_PICMG:            "PICMG",
+		OEM_RARITAN:          "Raritan", // 力登
+		OEM_KONTRON:          "Kontron", // 控创
+		OEM_PPS:              "PPS",
+		OEM_IBM_20301:        "IBM",
+		OEM_AMI:              "AMI",
+		OEM_FOXCONN:          "Foxconn",
+		OEM_ADLINK_24339:     "ADLINK", // 凌华
+		OEM_H3C:              "H3C",
+		OEM_NOKIA_28458:      "Nokia",
+		OEM_VITA:             "Vita",      // 维塔
+		OEM_INSPUR:           "Inspur",    // 浪潮
+		OEM_TENCENT:          "Tencent",   // 腾讯
+		OEM_BYTEDANCE:        "ByteDance", // 字节跳动
+		OEM_SUPERMICRO_47488: "Supermicro",
+		OEM_YADRO:            "Yadro",
 	}
 	if s, ok := m[oem]; ok {
 		return s
